4779a7ef8103e3bea03539b4222e4bfab1714aa4
Tommaso <tommasobertoldi00@gmail.com>
2018-10-24 15:25:33 +0200

    Update RichiestaJson/richiestaJson.js

--- RichiestaJson/richiestaJson.js
+++ RichiestaJson/richiestaJson.js
@@ -30,7 +30,7 @@ function sleep(ms){
 }
 async function ricarica() {
 	while(true){
-		await sleep(120000);
+		
 		request.get(url, function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 				var json = JSON.parse(body);
@@ -54,6 +54,7 @@ async function ricarica() {
 				});		
 			}
 		});
+		await sleep(120000);
 	}
 }
 app.listen(3000);
