956e1a68ca1bd560d7e6aeebc5dee37122c74218
Tommaso <tommasobertoldi00@gmail.com>
2018-10-24 15:22:39 +0200

    Update RichiestaJson/richiestaJson.js

--- RichiestaJson/richiestaJson.js
+++ RichiestaJson/richiestaJson.js
@@ -13,11 +13,7 @@ var app = express();
 var request = require('request');
 var schedule = require('node-schedule');
 var url = 'http://dati.venezia.it/sites/default/files/dataset/opendata/livello.json';
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('');
-	
-}).listen(8080); 
+
 
 function sleep(milliseconds) {
   var start = new Date().getTime();
@@ -27,41 +23,45 @@ function sleep(milliseconds) {
     }
   }
 }
+function sleep(ms){
+    return new Promise(resolve=>{
+        setTimeout(resolve,ms)
+    })
+}
+async function ricarica() {
+	while(true){
+		await sleep(120000);
+		request.get(url, function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				var json = JSON.parse(body);
+				var i;
+				for(i = 0; i< json.length && json[i].nome_abbr != "PS_Giud" ; i++);
+				console.log(json[i].nome_abbr);
+				//console.log(json[i].valore); 
 
-function ricarica() {
-	
-	request.get(url, function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			var json = JSON.parse(body);
-			var i;
-			for(i = 0; i< json.length && json[i].nome_abbr != "PS_Giud" ; i++);
-			//console.log(json[i].nome_abbr);
-			//console.log(json[i].valore); 
-						  console.log("attibuisco i permessi");
-
-			app.use(function(req, res, next) {
-				res.header("Access-Control-Allow-Origin", "*");
-				res.header("Access-Control-Allow-Headers", "Accept, Content-Type, If-None-Match, X-If-None-Match");
-				res.header("Access-Control-Expose-Headers", "Location, Warning, Etag");
-				res.header("Access-Control-Allow-Headers", "Content-Type, If-None-Match, X-If-None-Match, Authorization");
-				res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS");
+				app.use(function(req, res, next) {
+					res.header("Access-Control-Allow-Origin", "*");
+					res.header("Access-Control-Allow-Headers", "Accept, Content-Type, If-None-Match, X-If-None-Match");
+					res.header("Access-Control-Expose-Headers", "Location, Warning, Etag");
+					res.header("Access-Control-Allow-Headers", "Content-Type, If-None-Match, X-If-None-Match, Authorization");
+					res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS");
+					
+					next();
+				});
 				
-				next();
-			});
-			
-			app.get('/valore', function(req, res, next){
-			  res.json({ livello: json[i].valore }); 
-			  console.log("ricarico dal server");
-			});		
-		}
-	});
+				app.get('/valore', function(req, res, next){
+				  res.json({ livello: json[i].valore }); 
+				});		
+			}
+		});
+	}
 }
 app.listen(3000);
 
- let startTime = new Date(Date.now());
+/*  let startTime = new Date(Date.now()+30);
 let endTime = new Date(startTime.getTime() + 6000);
 var j = schedule.scheduleJob({ start: startTime, end: endTime, rule: '* * * * *' }, function(){
-			  console.log("Ricarica");
+			  
+}); */
 
-  ricarica();
-});
+	ricarica();
